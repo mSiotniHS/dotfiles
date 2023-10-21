e2c88589ee3a77bf6da5d5e887cadb942912fd09
Artyom Abramyan <artemabramyan7@icloud.com>
2023-10-21 20:30:41 +0300

    Add hostname to the prompt in zsh

--- .config/zsh/msiotnihs/settings.zsh
+++ .config/zsh/msiotnihs/settings.zsh
@@ -1,1 +1,7 @@
-PROMPT="┌[%~]"$'\n'"└ %# "
+# add colors
+autoload -U colors && colors
+
+PROMPT_LEFT="┌[$fg[green]%~$reset_color]"
+PROMPT_RIGHT="[$HOST]"
+PROMPT_PROMPT="└ λ "
+PROMPT=$(printf "%*s\r%s\n""$PROMPT_PROMPT" "$(tput cols)" "$PROMPT_RIGHT" "$PROMPT_LEFT")
